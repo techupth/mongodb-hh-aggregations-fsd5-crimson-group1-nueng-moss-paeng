3d3523822f1fa73e5a13d534e54a7faa09c7f534
pang <pangton28.p@gmail.com>
2024-01-11 17:50:47 +0700

    feat(exercise): add complete exercise

--- exercise-4.js
+++ exercise-4.js
@@ -1,1 +1,10 @@
 // Paste the complete MQL here
+db.pizzaOrders.aggregate([
+  {
+    $group: {
+      _id: { $year: "$created_at" },
+      total_amount: { $sum: "$total_price" },
+    },
+  },
+  { $sort: { _id: -1 } },
+]);
